89edaa74bf2eb84af7bda13704fe4c22ad6f070b
mini0-0 <nalala8200@naver.com>
2024-10-09 23:36:39 +0900

    계산기 웹 프로그램

--- was-practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -1,18 +1,21 @@
 package org.example;
 
-import org.example.calculator.domain.Calculator;
-import org.example.calculator.domain.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 
 public class CustomWebApplicationServer {
     private final int port;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
@@ -30,30 +33,11 @@ public class CustomWebApplicationServer {
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("Custom Web Application client connected ");
 
-                try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()){
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                    DataOutputStream dos = new DataOutputStream(out);
+                executorService.execute(new ClientRequestHandler(clientSocket));
 
-                    HttpRequest httpRequest = new HttpRequest(br);
-
-                    if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")){
-                        QueryStrings queryStrings = httpRequest.getQueryStrings();
-
-                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                        String operator = queryStrings.getValue("operator");
-                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-                        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
-                        byte[] body = String.valueOf(result).getBytes();
-
-                        HttpResponse response = new HttpResponse(dos);
-                        response.response200Header("application/json",body.length);
-                        response.responseBody(body);
-                        }
-                    }
-                }
             }
         }
 
+    }
 }
 
